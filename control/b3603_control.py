270abda2fbee809a2f0b539b015a715d92891a37
Vyacheslav Konovalov <konovalov@healbe.com>
2019-01-24 12:47:07 +0300

    fixed some bug with no response

--- control/b3603_control.py
+++ control/b3603_control.py
@@ -5,53 +5,60 @@ debug = 0  # Print debug message
 info_msg = 1  # Print info message
 
 class Control:
-    __port = serial.Serial()
-    connection = False  # Connection property
+    
 
     def __init__(self, port):
+        self.__port = serial.Serial()
         self.__port.baudrate = 38400
         self.__port.port = port
         self.__port.timeout = 1
+        self.__connection = False  # Connection property
         # Open Com
         try:
             self.__port.open()
-            print("Port is open")
-            self.connection = True            
+            print("B3603 " + self.__port.port + " port is open")
+            self.__connection = True            
         except IOError:
-            self.__connection_fail("Can't open port")
+            self.__close_connect("Can't open port")
+
 
     def __del__(self):
-        self.__connection_fail('')
+        self.__close_connect()
+    
     
     # Print info message
-    def _iprint(self, msg: str):
+    def __iprint(self, msg: str):
         if info_msg:
             print(msg)
 
-    def __connection_fail(self, msg: str):
+
+    def __close_connect(self, msg: str = 'B3603 Commander stop'):
         print(msg)
-        self.connection = False
+        self.__connection = False
         if self.__port.is_open:
-            self._iprint("Port close")
+            self.__iprint("B3603 " + self.__port.port + " port close")
             self.__port.close()
+
 
     # Send B3603 command
     # return: array of strings or array with zero lenght
     def send_cmd(self, cmd):
         ack = []
-        if self.connection == True:  # dummy protection
+        if self.__connection == True:  # dummy protection
             if cmd.endswith('\n') == False:
                 cmd = cmd + '\n'
             self.__port.write(cmd.encode())
-            self._iprint('B3603 Send cmd: ' + cmd.replace('\n', ''))
+            self.__iprint('B3603 Send cmd: ' + cmd.replace('\n', ''))
             ack = self.__read_ack()
             if debug:
                 self.print_ack(ack)
         if ack:
-            self._iprint('B3603 Cmd: OK' )
+            self.__iprint('B3603 Cmd: OK' )
         else:
-            self._iprint('B3603 Cmd: Error' )
-        return ack[:]
+            self.__iprint('B3603 Cmd: Error')
+            return 0
+        return ack
+
 
     # Print B3603 ACK
     def print_ack(self, data: []):
@@ -60,16 +67,26 @@ class Control:
             for s in data:
                 print(s)
 
+
+    # Get connection status
+    # return: true if connect
+    def get_status(self):
+        return self.__connection
+        
+        
     # Read B3603 ACK from port
     # return: array of strings or array with zero lenght
     def __read_ack(self):
-        for i in range(5):  # Waiting 500 ms
+        for i in range(5):  # Waiting 500 ms maximum
             if self.__port.in_waiting:
                 break
             time.sleep(.1)  # Waiting 100 ms
+        if self.__port.in_waiting:
+            print('Cmd was sent, but no response')
+            return
         data = []
         while self.__port.in_waiting:
             s: str = self.__port.readline().decode('utf-8')
             s = s.replace('\r\n', '')
             data.append(s)  # Remove \r\n
-        return data[:]
+        return data
--- control/b3603_test.py
+++ control/b3603_test.py
@@ -2,24 +2,25 @@
 from b3603_control import Control
 
 
-def end_connect(cmdr):
+def close_connect(cmdr):
     del cmdr
 
 
 def main():
     cmdr = Control('COM3')  # /dev/ttyUSB0 for Linux
-    if cmdr.connection == 0:
-        return end_connect(cmdr)
+    if cmdr.get_status() == 0:
+        return close_connect(cmdr)
     
     if cmdr.send_cmd("VOLTAGE 8000") == 0:
-        end_connect(cmdr)
+        return close_connect(cmdr)
     if cmdr.send_cmd("OUTPUT 1") == 0:
-        end_connect(cmdr)
+        return close_connect(cmdr)
     sleep(5)
     if cmdr.send_cmd("OUTPUT 0") == 0:
-        end_connect(cmdr)
+        return close_connect(cmdr)
         
-    end_connect(cmdr)
+    close_connect(cmdr)
+
 
 if __name__ == '__main__':
     main()
